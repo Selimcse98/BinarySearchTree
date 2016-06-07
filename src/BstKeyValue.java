da9abff5a3c927ea7238b7ae19f1f1b88ced98f4
Mohammad Selim Miah <selimcse98@gmail.com>
2016-06-08 01:48:29 +1000

    BST with Key, Value pair

--- src/BstKeyValue.java
+++ src/BstKeyValue.java
@@ -32,8 +32,59 @@ public class BstKeyValue {
         }
     }
 
-    public static void main(String args[]){
+    public void inOrderTraverseNode(Node focusNode){
+        if(focusNode!=null){
+            inOrderTraverseNode(focusNode.leftChild);
+            System.out.println(focusNode);
+            inOrderTraverseNode(focusNode.rightChild);
+        }
+    }
 
+    public void preOrderTraverseNode(Node focusNode){
+        if(focusNode!=null) {
+            System.out.println(focusNode);
+            preOrderTraverseNode(focusNode.leftChild);
+            preOrderTraverseNode(focusNode.rightChild);
+        }
+    }
+
+    public void postOrderTraverseNode(Node focusNode){
+        if(focusNode!=null){
+            postOrderTraverseNode(focusNode.leftChild);
+            postOrderTraverseNode(focusNode.rightChild);
+            System.out.println(focusNode);
+        }
+    }
+
+    public Node findNode(int key){
+        Node focusNode =root;
+
+        while (key!=focusNode.key){
+            if(key<focusNode.key)
+                focusNode = focusNode.leftChild;
+            else focusNode = focusNode.rightChild;
+
+            if(focusNode == null)
+                return null;
+        }
+        return focusNode;
+    }
+
+    public static void main(String args[]){
+        BstKeyValue bstKeyValue = new BstKeyValue();
+        bstKeyValue.addNode(50,"Boss");
+        bstKeyValue.addNode(25,"Vice President");
+        bstKeyValue.addNode(15,"Office Manager");
+        bstKeyValue.addNode(30,"Secretary");
+        bstKeyValue.addNode(75,"Sales Manager");
+        bstKeyValue.addNode(85,"Salesman 1");
+
+        //bstKeyValue.inOrderTraverseNode(bstKeyValue.root);
+        //bstKeyValue.preOrderTraverseNode(bstKeyValue.root);
+        bstKeyValue.postOrderTraverseNode(bstKeyValue.root);
+
+        System.out.println("\n\nSearch for Node 30: "+bstKeyValue.findNode(30));
+        System.out.println("\n\nSearch for Node 99: "+bstKeyValue.findNode(99));
     }
 
     class Node{
@@ -48,7 +99,7 @@ public class BstKeyValue {
         }
 
         public String toString(){
-            return "Key "+key+" has value "+value;
+            return key+" ==>> "+value;
         }
     }
 }
